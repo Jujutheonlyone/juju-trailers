2bf9ba00b15961d595d99ea5a087ead40bd39439
juliansunten <julian.sunten@eon.com>
2024-11-29 12:39:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JuJu Trailers',
+  webDir: 'www'
+};
+
+export default config;
