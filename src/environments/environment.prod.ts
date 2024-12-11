e2ae5264a325bd4cb4eed27bf0a4722d8cb6103c
Jujutheonlyone <julian.sunten95@gmail.com>
2024-12-11 14:14:20 +0100

    environment.prod.ts aktualisieren

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,10 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: true
+  production: false,
+  awsConfig: {
+    aws_project_region: 'YOUR_REGION',
+    aws_cognito_identity_pool_id: 'YOUR_IDENTITY_POOL_ID',
+    aws_cognito_region: 'YOUR_REGION',
+    aws_user_pools_id: 'YOUR_USER_POOLS_ID',
+    aws_user_pools_web_client_id: 'YOUR_WEB_CLIENT_ID',
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
