87c8c12a47cf6ef3df629c82e9ba4f54551ea994
juliansunten <julian.sunten@eon.com>
2024-12-12 00:24:24 +0100

    initial data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,47 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update",
-and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  User: a.model({
+    uid: a.id(),
+    username: a.string().required(),
+    email: a.string().required(),
+    vehicles: a.hasMany('Vehicle', 'uid'),
+  })
+    .identifier(['uid'])
+    .authorization((allow) => [allow.owner()]),
+
+  Vehicle: a.model({
+    vehicleId: a.id(),
+    plate: a.string().required(),
+    name: a.string().required(),
+    brand: a.string(),
+    description: a.string().required(),
+    status: a.enum(['available', 'rented', 'blocked']),
+    lockStatus: a.enum(['locked', 'unlocked', 'error']),
+    location: a.customType({
+      lat: a.float().required(),
+      long: a.float().required(),
+    }),
+    createdAt: a.datetime(),
+    updatedAt: a.datetime(),
+    vehicleType: a.enum(['suv', 'trailer']),
+    media: a.hasMany('VehicleMedia', 'vehicleId'),
+    user: a.belongsTo('User', 'uid'),
+    uid: a.id(),
+  })
+    .identifier(['vehicleId'])
+    .authorization((allow) => [allow.owner(), allow.group('Admin')]),
+
+  VehicleMedia: a.model({
+    mediaId: a.id(),
+    s3path: a.string().required(),
+    mediaType: a.enum(['video', 'image']),
+    alt: a.string(),
+    vehicle: a.belongsTo('Vehicle', 'vehicleId'),
+    vehicleId: a.id(),
+  })
+    .identifier(['mediaId'])
+    .authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -22,32 +52,3 @@ export const data = defineData({
     defaultAuthorizationMode: 'iam',
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
--- src/app/service/vehicle.service.ts
+++ src/app/service/vehicle.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Schema} from "../../../amplify/data/resource";
+import { generateClient } from 'aws-amplify/data'
+import { V6Client } from '@aws-amplify/api-graphql';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleService {
+  private client: V6Client<Schema> = generateClient<Schema>();
 
+  public async getAllVehicles(): Promise<Schema['Vehicle']['type'][]> {
+    const res = await this.client.models.Vehicle.list();
+    return res.data;
+  }
 }
