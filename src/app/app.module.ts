c10a9db426f93960b3d6472eb4b140d723ba24d4
juliansunten <julian.sunten@eon.com>
2024-12-18 21:15:07 +0000

    remove amplify configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import outputs from '../../amplify_outputs.json';
-import {Amplify} from "aws-amplify";
-
-Amplify.configure(outputs);
 
 
 @NgModule({
