b4decab713b8bcd731d0b4b78b429cd2bb4a6ea6
juliansunten <julian.sunten@eon.com>
2024-12-19 09:48:18 +0000

    initial setup

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,8 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import {trailerBooked} from "./trailer-booked/resource";
 
+
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
